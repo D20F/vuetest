185dca50ead1d343343a34e497d62aa94ec35938
fb <2442563148@qq.com>
2020-04-06 15:29:43 +0800

    vue仓库冲冲冲

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test :data='this.s' msg='Welcome to' @ccc="q"  ref="child"/>
+    <h1 @click="$refs.child.s(1)">click</h1>
+    <h1 >{{n}}</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test
+  },
+  data(){
+    return {
+        s:[{c:1},{c:2}],
+        n:1
+    }
+  },
+  methods:{
+    q(){
+      this.n += 1;
+    },
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ data[0].c }}</h1>
+    <h1>{{ data[1].c }}</h1>
+    <h1>{{ msg }}</h1>
+    <h1>t{{ n }}</h1>
+    <h1 @click="$emit('ccc')" >clickt</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: ['data','msg'],
+  data(){
+    return{
+      n:1
+    }
+  },
+  created(){
+
+
+  },
+  methods:{
+    s(data){
+      this.n += data
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;                              
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

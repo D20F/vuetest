ad173584c5d80b29f86f1d4933d3fc1475a1faad
fb <2442563148@qq.com>
2020-12-25 18:27:47 +0800

    dsadsa

--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="container_box">
         
+
+        <Login v-model="s" />
+        <p>{{s}}</p>
+
+
     </div>
 </template>
 
 <script>
+import Login from '../login/index'
 export default {
     name: "Home",
     data() {
-        return {};
+        return {
+            s:''
+        };
     },
-    components: {},
+    components: {
+        Login
+    },
     mixins: [],
     created() {},
     methods: {},
---
+++ src/view/login/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container_box">
+        <input type="text" v-model="filename" />
+        <p @click="emit">emit</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {};
+    },
+    props: {
+        value: {
+            type: String,
+            default: "",
+        },
+    },
+    components: {},
+    mixins: [],
+    created() {
+        
+    },
+    methods: {
+        emit() {
+            this.$emit("input", "ccccccc");
+        },
+    },
+    computed: {
+        filename: {
+            get() {
+                return this.value;
+            },
+            set(val) {
+                this.$emit("input", val);
+            },
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+</style>

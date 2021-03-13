aff64846f721f1fe0f1f047f12a83d96074ca8b3
fb <2442563148@qq.com>
2021-03-13 14:42:50 +0800

    修改路由

--- src/layout/component/footer/index.vue
+++ src/layout/component/footer/index.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="tabbar">
         <v-bottom-navigation height="50" v-model="value" grow color="primary">
-            <v-btn to="/" color="rgba(0, 0, 0, 0);" text height="100%">
+            <v-btn @click='jump("/")' color="rgba(0, 0, 0, 0);" text height="100%">
                 <span>设备</span>
 
                 <v-icon>mdi-dns</v-icon>
                 <!-- <img src="@/static/img/tabbar/equipment.png" alt=""> -->
             </v-btn>
-            <v-btn to="/income" text color="rgba(0, 0, 0, 0);" height="100%">
+            <v-btn @click='jump("/income")' text color="rgba(0, 0, 0, 0);" height="100%">
                 <span>收益</span>
 
                 <v-icon>mdi-wallet-outline</v-icon>
             </v-btn>
 
-            <v-btn to="/my" color="rgba(0, 0, 0, 0);" text height="100%">
+            <v-btn @click='jump("/my")' color="rgba(0, 0, 0, 0);" text height="100%">
                 <span>我的</span>
 
                 <v-icon>mdi-account</v-icon>
@@ -28,7 +28,13 @@ export default {
     mixins: [],
     data: () => ({ value: 1 }),
     computed: {},
-    methods: {},
+    methods: {
+        jump(path) {
+            this.$router.replace({
+                path: path,
+            });
+        },
+    },
 };
 </script>
 
